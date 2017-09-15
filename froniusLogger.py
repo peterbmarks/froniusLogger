c12c633a05ed19b8b0a95a35ec92be30f8bbbcda
Peter Marks <peter.marks@abc.net.au>
2017-09-16 03:31:38 +1000

    Handle the connection timeout that happens at night when the Fronius is asleep.

--- froniusLogger.py
+++ froniusLogger.py
@@ -12,14 +12,18 @@ import time
 host = "192.168.0.112"
 sample_seconds = 60  # how many seconds between samples, set to zero to run once and exit
 
+
 def main():
     print("started")
     while True:
-        watts = watts_generated()
-        now = time.strftime("%H:%M:%S")
-        line = "%s\t%s\n" % (now, watts)
-        # print(line)
-        write_to_logfile(line)
+        try:
+            watts = watts_generated()
+            now = time.strftime("%H:%M:%S")
+            line = "%s\t%s\n" % (now, watts)
+            # print(line)
+            write_to_logfile(line)
+        except requests.exceptions.ConnectTimeout:
+            print("Connect timeout")
         if sample_seconds > 0:
             time.sleep(sample_seconds)
         else:
@@ -36,7 +40,7 @@ def write_to_logfile(line):
 
 def watts_generated():
     url = "http://" + host + "/solar_api/v1/GetInverterRealtimeData.cgi?Scope=System"
-    r = requests.get(url, timeout=60)
+    r = requests.get(url, timeout=2)
     json_data = r.json()
     result = json_data["Body"]["Data"]["PAC"]["Values"]["1"]
     return result

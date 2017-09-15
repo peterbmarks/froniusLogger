c6dd9467b99f99929d208039b9e12ba2ac071192
Peter Marks <peter.marks@pobox.com>
2017-09-15 12:29:30 +1000

    Improved the formatting of the time x axis.

--- graph.py
+++ graph.py
@@ -9,8 +9,10 @@ On Fedora Linux: sudo dnf install python3-matplotlib
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
 import csv
 import time
+import datetime
 
 today = time.strftime("%Y_%m_%d")
 
@@ -18,24 +20,26 @@ INFILENAME = today + '.csv'
 
 
 def main():
-    datareader = csv.reader(open(INFILENAME,'r'), delimiter='\t')
+    data_reader = csv.reader(open(INFILENAME,'r'), delimiter='\t')
     y = []
     x = []
-    xlabels = []
-    counter = 0
-    for row in datareader:
+    for row in data_reader:
         if len(row) > 1:
             timeval = row[0]
-            print("timeval = %s" % timeval)
-            xlabels.append(timeval)
-            x.append(counter)
-            counter += 1
+
+            dt = datetime.datetime.strptime(timeval, "%H:%M:%S")
+            print("timeval = %s -> %s" % (timeval, dt))
+            x.append(mdates.date2num(dt))
             watts = float(row[1])
             y.append(watts)
 
-    plt.plot(y)
-    plt.xticks(x, xlabels, rotation=30)
-    plt.ylabel('Watt')
+    fig, ax = plt.subplots()
+    ax.plot(x,y)
+    ax.xaxis_date()
+    my_fmt = mdates.DateFormatter('%H:%M')
+    ax.xaxis.set_major_formatter(my_fmt)
+    plt.ylabel("Watts")
+    plt.xlabel("Time")
     plt.show()
     plt.savefig('%s_graph' % today)
 
